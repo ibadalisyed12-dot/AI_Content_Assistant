b38d5a94170d319c8a7eea76f3b488c74dd63332
Ibad <ibadalisyed12@gmail.com>
2026-09-22 23:37:44 +0500

    Change the model in the app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ with st.sidebar:
     # Popular free production model on Groq
     model_choice = st.selectbox(
     "Choose Groq Model",
-    ["llama-3.1-8b-instant", "openai/gpt-oss-20b"],
+    ["llama-3.1-8b-instant", "llama-3.3-70b-versatile"],
     index=0
 )
     
